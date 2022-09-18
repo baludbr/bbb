edb7cd5ef7c80e4cf488d41c271d57d7cfdad7e7
balaji <2100030140@kluniversity.in>
2022-09-18 12:43:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header';
+import Booking from './Components/Booking';
+import Shopping from './Components/Shopping';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import ContactUs from './Components/Contactus';
+import {Router,Routes,Route} from 'react-router-dom'
+import Checkout from './Components/Checkout';
+import IncomeAreaChart from './Components/OrderTable';
 function App() {
   return (
-    <div className="App">
-      <h1>Hiii</h1>
+    <div className='aa'>
+     <Header></Header>  
+  <div className="App">
+    <Routes>
+      <Route path='/' element={<IncomeAreaChart/>}/>
+      <Route path='/booking' element={<Checkout/>}/>
+      <Route path='/shopping' element={<Shopping/>}/>
+      <Route path='/Feedback' element={<ContactUs/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    <br/><br/>
+    </div>
     </div>
   );
 }
